0a78c797eafbbc91b82b65a30df4c37eab4fb310
marufhasan566 <marufhasan.iitju47@gmail.com>
2023-06-08 13:49:29 +0600

    practise

---
+++ dyanamic_array_2.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int *arr = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    // Print
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << "\t";
+    }
+    cout << endl
+         << *arr << " " << arr;
+
+    return 0;
+}
---
+++ dynamic_data_2.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int *a = new int;
+    float *b = new float;
+    cin >> *a >> *b;
+    cout << a << " " << *a << endl;
+    cout << b << " " << *b << endl;
+    return 0;
+}
